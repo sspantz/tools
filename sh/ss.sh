3b884ecce009e591ab46c9acdf7517507ea16a13
sspantz <sspantiezheng@gmail.com>
2019-03-06 14:07:17 +0800

    Update

--- sh/ss.sh
+++ sh/ss.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt update -y && apt upgrade -y
+apt update -y
 apt install golang -y
 go get git@github.com:shadowsocks/shadowsocks-go.git
 wget https://raw.githubusercontent.com/sspantz/tools/master/sh/shadowsocks-server.service
