300bdcf9152aed00ab483879b791a62d2f9f8acd
sspantz <sspantiezheng@gmail.com>
2019-03-06 14:36:58 +0800

    Update

--- sh/ss.sh
+++ sh/ss.sh
@@ -10,12 +10,14 @@ echo \", >> config.json
 echo \"local_address\":\"127.0.0.1\", >> config.json
 echo \"port_password\": { >> config.json
 echo \"8387\": \"159159!\", >> config.json
-echo \"8388\": \"147147!\" >> config.json
+echo \"8388\": \"147147!\", >> config.json
 echo \"8389\": \"123123!\" >> config.json
 echo    }, >> config.json
 echo \"method\": \"aes-128-cfb\", >> config.json
 echo \"timeout\":600 >> config.json
 echo } >> config.json
 cp shadowsocks-server.service /etc/systemd/system/
-systemctl enbale shadowsocks-server
+systemctl enable shadowsocks-server
 systemctl restart shadowsocks-server
+echo "Please setting your client with the follow json file."
+cat config.json
