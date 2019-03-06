4dca44843cc218bd2c7ce88ec856069d54cfce6c
sspantz <sspantiezheng@gmail.com>
2019-03-06 13:59:00 +0800

    Update ss.sh

--- sh/ss.sh
+++ sh/ss.sh
@@ -1,6 +1,20 @@
-#!/usr/bin/sh
-
+#!/bin/sh
 apt update -y && apt upgrade -y
 apt install golang -y
 go get git@github.com:shadowsocks/shadowsocks-go.git
-wget https://raw.githubusercontent.com/sspantz/tools/master/README.md
+wget https://raw.githubusercontent.com/sspantz/tools/master/sh/shadowsocks-server.service
+echo { >> config.json
+echo -n \"server\":\" >> config.json
+echo -n $(/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:") >> config.json
+echo \", >> config.json
+echo \"local_address\":\"127.0.0.1\", >> config.json
+echo \"port_password\": { >> config.json
+echo \"8387\": \"159159!\", >> config.json
+echo \"8388\": \"147147!\", >> config.json
+echo    }, >> config.json
+echo \"method\": \"aes-128-cfb\", >> config.json
+echo \"timeout\":600 >> config.json
+echo } >> config.json
+cp shadowsocks-server.service /etc/systemd/system/
+systemctl enbale shadowsocks-server
+shut -r +0
