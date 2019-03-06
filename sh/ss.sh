7146da532a317c1d28ec85e42e80bdec10f1a5ec
sspantz <sspantiezheng@gmail.com>
2019-03-06 14:26:07 +0800

    Update

--- sh/ss.sh
+++ sh/ss.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 apt update -y
 apt install golang -y
-go get git@github.com:shadowsocks/shadowsocks-go.git
+go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
 wget https://raw.githubusercontent.com/sspantz/tools/master/sh/shadowsocks-server.service
-echo { >> config.json
+echo { > config.json
 echo -n \"server\":\" >> config.json
 echo -n $(/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:") >> config.json
 echo \", >> config.json
 echo \"local_address\":\"127.0.0.1\", >> config.json
 echo \"port_password\": { >> config.json
 echo \"8387\": \"159159!\", >> config.json
-echo \"8388\": \"147147!\", >> config.json
+echo \"8388\": \"147147!\" >> config.json
+echo \"8389\": \"123123!\" >> config.json
 echo    }, >> config.json
 echo \"method\": \"aes-128-cfb\", >> config.json
 echo \"timeout\":600 >> config.json
 echo } >> config.json
 cp shadowsocks-server.service /etc/systemd/system/
 systemctl enbale shadowsocks-server
-shutdown -r +0
+systemctl restart shadowsocks-server
